7b57d46d7dc6d5855b2d81f8d7daede2f060297a
Augusto Telles <40339166+toguvr@users.noreply.github.com>
2020-03-13 12:32:07 -0300

    inclusindo variaveis ambiente

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,23 +1,22 @@
-import jwt from 'jsonwebtoken'
-import authConfig from '../../config/auth'
-import {promisify} from 'util'
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import authConfig from '../../config/auth';
 
-export default async (req,res, next)=>{
-  const authHeader = req.headers.authorization
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
 
-  if(!authHeader){
-    return res.status(401).json({error: 'Token not provided'})
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
   }
 
-  const [,token] = authHeader.split(' ')
+  const [, token] = authHeader.split(' ');
 
-  try{
-    const decoded = await promisify(jwt.verify)(token, authConfig.secret)
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
-    req.userId = decoded.id
-    return next()
-  }catch (err){
-    return res.status(401).json({error: 'Token invalid'})
+    req.userId = decoded.id;
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
   }
-
-}
+};
--- src/database/seeds/20200130144000-admin-user.js
+++ src/database/seeds/20200130144000-admin-user.js
@@ -1,21 +1,21 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 
 module.exports = {
   up: QueryInterface => {
     return QueryInterface.bulkInsert(
-      "users",
+      'users',
       [
         {
-          name: "Distruidora FastFeet",
-          email: "admin@fastfeet.com",
-          password_hash: bcrypt.hashSync("123456", 8),
+          name: 'Distruidora FastFeet',
+          email: 'admin@fastfeet.com',
+          password_hash: bcrypt.hashSync('123456', 8),
           created_at: new Date(),
-          updated_at: new Date()
-        }
+          updated_at: new Date(),
+        },
       ],
       {}
     );
   },
 
-  down: () => {}
+  down: () => {},
 };
